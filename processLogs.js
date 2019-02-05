c4f21f37e15c55f7450e075ce84226e639f208f5
Kian Mayne <git@kianmayne.co.uk>
2019-02-05 22:21:25 +0000

    Attempted to build parallelism

--- processLogs.js
+++ processLogs.js
@@ -1,15 +1,15 @@
 #!/usr/bin/env node
 
 const Alpine = require('alpine');
+const os = require('os');
 const fs = require('fs').promises;
 const { Readable } = require('stream');
 const readline = require('readline');
-const { fork } = require('child_process');
-
+const { spawn } = require('child_process');
 
 const ENCODING = 'utf8';
-const NO_DATA_MESSAGE_TIMEOUT_MS = 2000;
-const NUM_THREADS = System.Environment.ProcessorCount;
+const NO_DATA_MESSAGE_TIMEOUT_MS = 200000;
+const NUM_THREADS = os.cpus().length;
 
 // Set up standard input encoding
 process.stdin.setEncoding(ENCODING);
@@ -24,18 +24,9 @@ const noDataMessage = setInterval(() => {
 }, NO_DATA_MESSAGE_TIMEOUT_MS);
 process.stdin.once('data', () => clearInterval(noDataMessage));
 
-if (process.argv.length < 1) {
+if (process.argv.length < 3) {
   console.error('No ruleset passed to log processor.');
   process.exit();
-}
-
-async function readRules(rulesFile) {
-  const rawRules = await fs.readFile(rulesFile, ENCODING);
-  return [{
-    regexString: '.',
-    message: '[TEST RULE] Marks every item as suspicious',
-    severity: 'CRITICAL'
-  }];
 }
 
 class ApacheLogEntryStream extends Readable {
@@ -54,19 +45,46 @@ class ApacheLogEntryStream extends Readable {
 
 const entryStream = new ApacheLogEntryStream(process.stdin);
 
-readRules(process.argv[0]).then(rules => {
-  const rulePool = rules.slice(NUM_THREADS);
-  for (let i = 0; i < NUM_THREADS; i++) {
-    const rule = rules[i];
-    spawnRegexProc(rule, rulePool);
-  }
-});
+fs.readFile(process.argv[2], ENCODING)
+  .then(parseRules)
+  .then(rules => {
+    const rulePool = rules.slice(NUM_THREADS);
+    for (let i = 0; i < NUM_THREADS; i++) {
+      const rule = rules[i];
+      spawnRegexProc(rule, rulePool);
+    }
+  })
+  .catch(err => {
+    console.error('Error occurred while processing rules: ', err);
+  });
 
+let finishedProcs = 0;
 function spawnRegexProc(rule, pool) {
-  const ruleProc = fork('./runRegex', [rule.regexString], { stdio: [entryStream, 'inherit', 'inherit'] });
+  const ruleProc = spawn(process.execPath, ['./runRegex', rule.regexString]);
   ruleProc.on('exit', code => {
     if (code !== 0) console.error(`Regex proc with rule "${rule.regex}" failed with exit code ${code}.`);
-    spawnRegexProc(pool.pop(), pool);
+    if (pool.length !== 0) {
+      spawnRegexProc(pool.pop(), pool);
+    } else {
+      finishedProcs++;
+      if (finishedProcs === NUM_THREADS) {
+        // All processing complete
+      }
+    }
   });
+
+  entryStream.pipe(ruleProc.stdin);
+  ruleProc.stdout.pipe(process.stdout);
 }
 
+function parseRules(ruleFileString) {
+  return ruleFileString.replace(/^(#.*|\s*)\n?/gm, '') // Remove comments
+    .replace(/\\\n/gm, ' ') // Remove escaped new lines
+    .split('\n')
+    .filter(rule => /"@rx ((?:[^"\\]|\\.)*)"/g.exec(rule))
+    .map(rule => ({
+      regexString: /"@rx ((?:[^"\\]|\\.)*)"/g.exec(rule)[1].replace(/\(\?i:/g, '(?:').replace(/\(\?i\)/g, '').replace(/\+\+/g, '+'),
+      message: /msg:'([^']+)'/ig.exec(rule) ? /msg:'([^']+)'/ig.exec(rule)[1] : '',
+      severity: /severity:'([^']+)'/ig.exec(rule) ? /severity:'([^']+)'/ig.exec(rule)[1] : '',
+    }));
+}
--- runRegex.js
+++ runRegex.js
@@ -1,2 +1,9 @@
-const regexStr = process.argv[0];
+const regexStr = process.argv[2];
 
+process.stdin.pipe(new (require('stream').Transform)({
+  // writableObjectMode: true,
+  transform(chunk, encoding, callback) {
+    callback(chunk);
+    // return callback(JSON.stringify(chunk));
+  }
+})).pipe(process.stdout);
