ad354e5015db07d8d4f6766c3893e4ee3d2d2252
Kian Mayne <git@kianmayne.co.uk>
2019-02-06 15:58:05 +0000

    Removed concurrency code & implemented sync sig checking

--- processLogs.js
+++ processLogs.js
@@ -1,15 +1,12 @@
 #!/usr/bin/env node
 
 const Alpine = require('alpine');
-const os = require('os');
 const fs = require('fs').promises;
-const { Readable } = require('stream');
+const { Readable, Transform } = require('stream');
 const readline = require('readline');
-const { spawn } = require('child_process');
 
 const ENCODING = 'utf8';
 const NO_DATA_MESSAGE_TIMEOUT_MS = 200000;
-const NUM_THREADS = os.cpus().length;
 
 // Set up standard input encoding
 process.stdin.setEncoding(ENCODING);
@@ -43,48 +40,58 @@ class ApacheLogEntryStream extends Readable {
   }
 }
 
-const entryStream = new ApacheLogEntryStream(process.stdin);
-
-fs.readFile(process.argv[2], ENCODING)
-  .then(parseRules)
-  .then(rules => {
-    const rulePool = rules.slice(NUM_THREADS);
-    for (let i = 0; i < NUM_THREADS; i++) {
-      const rule = rules[i];
-      spawnRegexProc(rule, rulePool);
-    }
-  })
-  .catch(err => {
-    console.error('Error occurred while processing rules: ', err);
-  });
-
-let finishedProcs = 0;
-function spawnRegexProc(rule, pool) {
-  const ruleProc = spawn(process.execPath, ['./runRegex', rule.regexString]);
-  ruleProc.on('exit', code => {
-    if (code !== 0) console.error(`Regex proc with rule "${rule.regex}" failed with exit code ${code}.`);
-    if (pool.length !== 0) {
-      spawnRegexProc(pool.pop(), pool);
-    } else {
-      finishedProcs++;
-      if (finishedProcs === NUM_THREADS) {
-        // All processing complete
-      }
-    }
-  });
-
-  entryStream.pipe(ruleProc.stdin);
-  ruleProc.stdout.pipe(process.stdout);
-}
-
 function parseRules(ruleFileString) {
   return ruleFileString.replace(/^(#.*|\s*)\n?/gm, '') // Remove comments
     .replace(/\\\n/gm, ' ') // Remove escaped new lines
     .split('\n')
     .filter(rule => /"@rx ((?:[^"\\]|\\.)*)"/g.exec(rule))
     .map(rule => ({
-      regexString: /"@rx ((?:[^"\\]|\\.)*)"/g.exec(rule)[1].replace(/\(\?i:/g, '(?:').replace(/\(\?i\)/g, '').replace(/\+\+/g, '+'),
-      message: /msg:'([^']+)'/ig.exec(rule) ? /msg:'([^']+)'/ig.exec(rule)[1] : '',
-      severity: /severity:'([^']+)'/ig.exec(rule) ? /severity:'([^']+)'/ig.exec(rule)[1] : '',
+      regex: new RegExp(/"@rx ((?:[^"\\]|\\.)*)"/g.exec(rule)[1].replace(/\(\?i:/g, '(?:').replace(/\(\?i\)/g, '').replace(/\+\+/g, '+')),
+      message: /msg:'([^']+)'/ig.exec(rule) ? /msg:'([^']+)'/ig.exec(rule)[1] : '[NO MESSAGE PROVIDED]',
+      severity: /severity:'([^']+)'/ig.exec(rule) ? /severity:'([^']+)'/ig.exec(rule)[1] : '[NO SEVERITY PROVIDED]',
     }));
 }
+
+
+class ObjectToJSONTranformStream extends Transform {
+  constructor() {
+    super({
+      writableObjectMode: true,
+      transform(obj, encoding, callback) {
+        return callback(null, JSON.stringify(obj));
+      }
+    });
+  }
+}
+
+class RegexRuleCheckingStream extends Transform {
+  constructor(rules) {
+    super({
+      objectMode: true,
+      transform(entry, encoding, callback) {
+        this.runRules(entry).forEach(matchedRule => this.push(matchedRule));
+        callback();
+      }
+    });
+    this.rules = rules;
+  }
+
+  runRules(entry) {
+    return this.rules.filter(rule => rule.regex.test(entry.originalLine));
+  }
+}
+
+function processLogs(rulesFile) {
+  const entryStream = new ApacheLogEntryStream(process.stdin);
+  fs.readFile(rulesFile, ENCODING)
+    .then(parseRules)
+    .then(rules => entryStream
+      .pipe(new RegexRuleCheckingStream(rules))
+      .pipe(new ObjectToJSONTranformStream())
+      .pipe(process.stdout))
+    .catch(err => {
+      console.error('Error occurred while processing rules: ', err);
+    });
+}
+
+processLogs(process.argv[2]);
--- runRegex.js
+++
@@ -1,9 +0,0 @@
-const regexStr = process.argv[2];
-
-process.stdin.pipe(new (require('stream').Transform)({
-  // writableObjectMode: true,
-  transform(chunk, encoding, callback) {
-    callback(chunk);
-    // return callback(JSON.stringify(chunk));
-  }
-})).pipe(process.stdout);
